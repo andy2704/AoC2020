700f6e1c209a5a3fa9b90e4ab29d886490a2d367
andyk <andreas.koernig.1988@gamil.com>
2020-12-14 23:24:14 +0100

    wip

--- src/com/company/Day7.java
+++ src/com/company/Day7.java
@@ -1,26 +1,47 @@
 package com.company;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.TreeMap;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day7 {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
-        TreeMap<Integer, String> tree = new TreeMap<>();
-        String row = reader.readLine();
+        Scanner scanner = new Scanner(new FileReader("input.txt"));
+        HashMap<String, List> bags = new HashMap<>(); //no order
+        Pattern parentPattern = Pattern.compile("\\w+ \\w+"); //regex one word, one word
+        Pattern childPattern = Pattern.compile("\\d+ \\w+ \\w+"); //regex one number, one word, one word
+        int count = 0;
 
-        while (row != null) {
-            row = "1 " + row;
-            String[] splitted = row.split("bags contain\\s*|bag,\\s*|bags,\\s*|bag.\\s*|bags.\\s*");
+        while (scanner.hasNext()) {
+            String row = scanner.nextLine();
+            Matcher parentMatcher = parentPattern.matcher(row); //match the regex against the string
+            parentMatcher.find();
+            String parent = parentMatcher.group();
 
-            for (String s : splitted) {
-                System.out.println(s);
+            Matcher childMatcher = childPattern.matcher(row);
+            List<String> child = new LinkedList<>();
+            while (childMatcher.find()) {
+                child.add(childMatcher.group());
             }
-            System.out.println(splitted.length);
-            row = reader.readLine();
+            bags.put(parent, child);
         }
-        reader.close();
+
+        //System.out.println("parent = " + parent);
+        //System.out.println("child = " + child);
+        System.out.println(bags);
+
+        for (Map.Entry<String, List> entry : bags.entrySet()) {
+            String k = entry.getKey();
+            List v = entry.getValue();
+            if (Pattern.matches(".*shiny gold.*", entry.getValue().toString())){ //true if "shiny gold" is found anywhere in the value
+                System.out.println("key: " + k + ", value: " + v);
+                count++;
+            }
+        }
+
+        System.out.println("count = " + count);
+        scanner.close();
     }
 }
