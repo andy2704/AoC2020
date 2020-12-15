2beb2cf30dd37225e9f867fe8aa5ab0b2fc4d370
andyk <andreas.koernig.1988@gamil.com>
2020-12-15 14:20:13 +0100

    Day7 rdy

--- src/com/company/Day7.java
+++ src/com/company/Day7.java
@@ -27,20 +27,80 @@ public class Day7 {
             }
             bags.put(parent, child);
         }
+        //System.out.println(bags);
 
-        //System.out.println("parent = " + parent);
-        //System.out.println("child = " + child);
-        System.out.println(bags);
+//------------------------------Part Two-------------------------------
+        //build searchQueue
 
-        for (Map.Entry<String, List> entry : bags.entrySet()) {
-            String k = entry.getKey();
-            List v = entry.getValue();
-            if (Pattern.matches(".*shiny gold.*", entry.getValue().toString())){ //true if "shiny gold" is found anywhere in the value
-                System.out.println("key: " + k + ", value: " + v);
-                count++;
+        Pattern childNumberPattern = Pattern.compile("\\d+");
+        Pattern childNewParentPattern = Pattern.compile("[a-zA-Z]+ [a-zA-Z]+");
+        Queue<String> searchQ = new LinkedList<>();
+
+        for (Object child : bags.get("shiny gold")) { //get key 'shiny gold' and look for all direct childs
+            Matcher childNumberMatcher = childNumberPattern.matcher(child.toString());
+            childNumberMatcher.find();
+
+            Matcher childNewParentMatcher = childNewParentPattern.matcher(child.toString());
+            childNewParentMatcher.find();
+
+            int number = Integer.parseInt(childNumberMatcher.group());
+            String childColor = childNewParentMatcher.group();
+
+            count+= number;
+
+            for (int i = 0; i < number; i++) {
+                searchQ.add(childColor); //color in Queue = number found
             }
         }
 
+        // go through the tree and count the numbers
+        while (!searchQ.isEmpty()) {
+            String nextSearch = searchQ.poll();
+            for (Object child : bags.get(nextSearch)) {
+                Matcher childNumberMatcher = childNumberPattern.matcher(child.toString());
+                childNumberMatcher.find();
+
+                Matcher childNewParentMatcher = childNewParentPattern.matcher(child.toString());
+                childNewParentMatcher.find();
+
+                int number = Integer.parseInt(childNumberMatcher.group());
+                String childColor = childNewParentMatcher.group();
+
+                count+= number;
+
+                for (int i = 0; i < number; i++) {
+                    searchQ.add(childColor); //color in Queue = number found
+                }
+            }
+        }
+
+/*--------------------------------Part One--------------------------------
+
+        Queue<String> searchQueue = new LinkedList<>();
+        for (Map.Entry<String, List> entry : bags.entrySet()) {
+            String k = entry.getKey();
+            //List v = entry.getValue();
+            if (Pattern.matches(".*shiny gold.*", entry.getValue().toString())){ //true if "shiny gold" is found anywhere in the value
+                //System.out.println("key: " + k + ", value: " + v);
+                searchQueue.add(k);
+                bags.put(k, null);
+                count++;
+            }
+        }
+        while (!searchQueue.isEmpty()){
+            String nextSearch = searchQueue.poll();
+            for (Map.Entry<String, List> entry : bags.entrySet()) {
+                String k = entry.getKey();
+                //List v = entry.getValue();
+                if (entry.getValue() != null &&  Pattern.matches(".*" + nextSearch + ".*", entry.getValue().toString())){
+                    //System.out.println("key: " + k + ", value: " + v);
+                    searchQueue.add(k);
+                    bags.put(k, null);
+                    count++;
+                }
+            }
+        }
+*/
         System.out.println("count = " + count);
         scanner.close();
     }
